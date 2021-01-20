9ac7a947e2a34d42e50541c8c44e533fc4b372c8
PHimmel <peterhimmelman@yahoo.com>
2021-01-20 00:31:58 -0500

    init

---
+++ basic_scraper.py
@@ -0,0 +1,69 @@
+from bs4 import BeautifulSoup as Soup
+import urllib
+from urllib import request
+
+
+class Http:
+    def __init__(self, link):
+        self.link = link
+        self.raw_html = self.get_raw_html()
+        self.html = self.read_raw_html()
+
+    def get_raw_html(self):
+        return urllib.request.urlopen(self.link)
+
+    def read_raw_html(self):
+        return self.raw_html.read()
+
+
+class Parser(Http):
+    def __init__(self, link, class_filter):
+        super().__init__(link)
+        self.class_filter = class_filter
+        self.soup = self.make_soup()
+        self.text = self.extract_text()
+
+    def make_soup(self):
+        return Soup(self.html, 'html.parser')
+
+    def extract_text(self):
+        return [title.text for title in self.soup('a', class_=self.class_filter) if len(title.text) > 15]
+
+
+class Output:
+    def __init__(self):
+        self.text = Parser().extract_text()
+
+    def write_text_to_temp_file(self):
+        with open('/home/pi/Programs/Output/Documents/txt/news_headlines.txt', 'w') as file:
+            file.write(' »»» '.join(self.text))
+            file.close()
+
+    def return_text(self):
+        return ' >>> '.join(self.text)
+
+
+# returns full page headlines
+def get_headlines(news_source=None):
+    bbc = 'https://www.bbc.com/news'
+    bbc_class = 'gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-pica-bold nw-o-link-split__anchor'
+
+    yahoo = 'https://www.news.yahoo.com'
+    yahoo_class = 'Fw(b)'
+
+    if news_source == 'yahoo':
+        return Parser(yahoo, yahoo_class).extract_text()
+
+    elif news_source == 'bbc':
+        return Parser(bbc, bbc_class).extract_text()
+
+    else:
+        return Parser(bbc, bbc_class).extract_text()
+
+def main():
+    print('main module, main function is empty')
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ single_matrix_control.py
@@ -0,0 +1,286 @@
+"""
+A program for controlling LED matrices. Offers the ability to have continuous, autonomous operation
+as well as a simple interface to rapidly switch to other settings/functions.
+
+NOTES NOT ON RPI VERSION:
+
+    # make scroll_weather more flexible, can be reused as a .sh for many other tasks
+
+    The implementation layer is within the Bash class. All of the it's child classes only server to organize and
+    create the flow of it's logic. They also offer a clear and concise interface for accessing specific functionality
+    derived from Bash.
+
+    Bash is mostly build off of the hzeller rpi-rgb-led-matrix library --> it's API
+    is solely through the CLI, this is why Bash contains bash commands and interfaces with the terminal through the
+    subprocess python-standard library.
+
+    the actual running script is centralized in the main function.
+
+
+
+    ### USE THE PYTHON BINDINGS TO BUILD OFF OF THE RBG LIBRARY!!!!
+
+"""
+
+from subprocess import call, Popen, DEVNULL
+from time import sleep
+from datetime import datetime
+from random import randint
+import basic_scraper
+
+class Bash:
+
+    def __init__(self):
+        self.get_weather = 'WEATHER=$(curl wttr.in/13413?format="%C+%t+%hRH")'
+
+        self.scroll_weather = 'sudo ./scrolling-text-example $WEATHER -f ../fonts/peters_myfont.bdf --led-cols=64 ' \
+                              '--led-slowdown-gpio=3 -y-5 -b50 -s2 -l2 -C50,127,168'
+
+        self.scroll_news = 'sudo ./scrolling-text-example $NEWS -f ../fonts/peters_myfont.bdf --led-pwm-bits=6 --led-cols=64 --led-slowdown-gpio=4 -B21,17,122 -C201,64,10 -y-5 -s2 -b60 -l1'
+
+        self.text = 'sudo ./scrolling-text-example 30 Minute Update! -f ../fonts/peters_myfont.bdf --led-cols=64 ' \
+                    '--led-slowdown-gpio=2 -b 50 -s2 -y-4 -B200,10,53 -l2'
+
+        self.stop_matrix = 'ps -ef|grep led | grep -v grep | awk \'{print $2}\'| sudo xargs kill'
+
+        self.led_example_directory_path = '/home/pi/Programs/rpi-rgb-led-matrix/examples-api-use'
+
+    def _call(self, command):
+        call('{} > /dev/null 2>&1'.format(command), cwd=self.led_example_directory_path, shell=True)
+
+    def _popen(self, command):
+        Popen(command.split(' '), cwd=self.led_example_directory_path, stdout=DEVNULL)
+
+    def two_commands(self, one, two):
+        call('{} > /dev/null 2>&1 ; {} > /dev/null 2>&1'.format(one, two), cwd=self.led_example_directory_path, shell=True)
+
+    def kill_matrix(self):
+        self._call(self.stop_matrix)
+
+    def clock(self, settings):
+        self._popen(settings)
+
+    def weather(self):
+        self.two_commands(self.get_weather, self.scroll_weather)
+
+    def scrolling_text(self):
+        self._call(self.text)
+
+ #   def set_envir_var(self, name, data):
+ #       self._call('{}={}'.format(name, data))
+
+    def news_headlines(self, ENV_NAME, data):
+        set_var = '{}=\"BBC: {}\"'.format(ENV_NAME, data)
+        self.two_commands(set_var, self.scroll_news)
+
+    def demo_number(self, number, time):
+        self._call('sudo ./demo -D{} -t{} --led-cols=64 --led-rows=32 --led-brightness=50 --led-slowdown-gpio=2 -m 45'.format(number,
+                                                                                                                time))
+
+
+"""
+Clock offers user-specified default settings for the operation of an on-going clock display.
+"""
+
+
+class Clock(Bash):
+    single_clock = 'sudo ./clock -f ../fonts/9x18B.bdf -d %-1I:%M_%p --led-cols=64 --led-slowdown-gpio=3 -y 7 '
+    double_clock = 'sudo ./clock -f ../fonts/9x18B.bdf -d %I:%M%p --led-cols=64 --led-slowdown-gpio=3 -y 7 '
+    # nice ASCII --->   » ‗ ┼
+
+    def __init__(self):
+        super().__init__()
+        self.message = Message()
+        self.headlines = None
+        self.target_minute = 15
+        self.target_hour = 9
+        self.hour, self.minute, self.second = get_current_hour_minute_second()
+        self.regular_night = False
+
+    # sets clock formatting based on number of positions required
+    def set_clock(self, settings):
+        if 0 < self.hour < 10 or 12 < self.hour < 22:
+            self.clock(Clock.single_clock + settings)
+            return
+        self.clock(Clock.double_clock + settings)
+
+    def night_clock(self):
+        self.kill_matrix()
+
+        self.set_clock('-C 115,50,122 -b 2')
+        self.set_sleep_till_hour()
+
+    def set_sleep(self):
+        # process start time
+        start_secs = self.minute * 60 + self.second
+
+        # current time
+        now_secs = datetime.now().minute * 60 + datetime.now().second
+
+        # time elapsed during process
+        start_secs = now_secs - start_secs
+
+        # total time until wake
+        target_secs = self.target_minute * 60
+
+        # secconds between process start and wake (ULTIMATELY ROUNDS TO THE NEAREST MINUTE)
+        difference = target_secs - start_secs
+
+        # sleep occurs in this method
+        sleep(difference)
+
+    def set_sleep_till_hour(self):
+        till_hour = self.target_hour - self.hour - 1
+
+        if till_hour < 0:
+            till_hour = 24 - self.hour + self.target_hour - 1
+
+        sleep_secs = (till_hour * 3600) + ((60 - self.minute) * 60)
+        sleep(sleep_secs)
+
+    def set_first_sleep(self):
+        minutes = [min for min in range(self.minute, self.minute + self.target_minute) if min % self.target_minute == 0]
+
+        if int(minutes[0]) - 1 <= 0:
+            minutes += self.target_minute
+
+        target_minutes = int(minutes[0]) - self.minute
+        target_seconds = 60 - self.second
+
+        slumber = (target_minutes - 1) * 60 + target_seconds
+        sleep(slumber)
+
+    def set_rand_color_clock_and_sleep(self):
+        self.set_clock(rand_color())
+        self.set_sleep()
+
+    # set to display messages at the top of each hour during the day ---takes n headlines from main list to display, deletes after for continuous new headline ticker
+    # if it runsn through the total list - a new list is generated
+
+    def run_messages_with_headlines(self):
+        if len(self.headlines) != 0:
+            try:
+                self.message.run_messages(' >>> '.join(self.headlines[0:5]))
+                del(self.headlines[0:5])
+            except:
+                self.update_headlines_and_run_messages()
+        else:
+            sekf.update_headlines_and_run_messages()
+
+    def get_headlines(self):
+        self.headlines =  basic_scraper.get_headlines()
+        return self.headlines
+
+    def update_headlines_and_run_messages(self):
+        self.get_headlines()
+        self.run_messages_with_headlines()
+
+    def run_messages_without_headlines():
+        pass
+
+
+
+# This method currently serves as main for running the clock/total based on time.
+# Bash.kill_matrix() is needed immediately after any clock display. All other matrix commands clear themselves after 1) time expiration 2) data served fully.
+# The clock is a continuous daemon that must be explicitly turned off before anything can be displayed. Failure to do so will result in a completely unresponsive/error-state matrix
+# the matrix requires atleast ~3 seconds of sleep between one command and one immediately afterwards
+
+    def run_clock(self):
+        try:
+            # get list of all h3 (main) headline titles from BBC news
+            self.get_headlines()
+            start = True
+
+            while True:
+                self.hour, self.minute, self.second = get_current_hour_minute_second()
+
+                # initialization
+                if start is True:
+                    self.set_clock(rand_color())
+                    start = False
+                    self.set_first_sleep()
+                    continue
+
+                # night settings
+                if self.regular_night is True:
+                    if self.hour > 21 or self.hour <= self.target_hour:
+                        self.night_clock()
+                        continue
+                else:
+                    if 1 < self.hour <= self.target_hour:
+                        self.night_clock
+                        continue
+
+                # day settings
+                if self.minute % 30 == 0:
+                    self.kill_matrix()
+
+                    self.run_messages_with_headlines()
+                    # run specific demo for fixed period of time
+                    Graphics().demo_number(10, 40)
+                    self.set_rand_color_clock_and_sleep()
+                    continue
+
+                elif self.minute % self.target_minute == 0:
+                    self.kill_matrix()
+
+                    self.set_rand_color_clock_and_sleep()
+                    continue
+
+                else:
+                    continue
+
+        # IDEA: add a goodbye message here
+        except KeyboardInterrupt:
+            self.kill_matrix()
+            return
+
+
+class Message(Bash):
+
+    def __init__(self):
+        super().__init__()
+
+    # run_message is the 'main' method of Message.
+    def run_messages(self, headlines):
+        self.new_hour()
+        self.display_weather()
+        self.news(headlines)
+
+    def display_weather(self):
+        self.weather()
+
+    def new_hour(self):
+        self.scrolling_text()
+
+    def news(self, headlines):
+        self.news_headlines('NEWS', headlines)
+
+class Graphics(Bash):
+    def __init__(self):
+        super().__init__()
+
+    def demo(self, number, time):
+        self.demo_number(number, time)
+
+
+class Image:
+    pass
+
+def get_current_hour_minute_second():
+    now = datetime.now()
+    return now.hour, now.minute, now.second
+
+def rand_color():
+    return '-C {},{},{} -b 60'.format(randint(0, 255), randint(0, 255), randint(0, 255))
+
+def main():
+    clock = Clock()
+    clock.run_clock()
+
+
+if __name__ == '__main__':
+    main()
+else:
+    print('Imported')
+

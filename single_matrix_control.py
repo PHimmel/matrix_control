b737310b39ff767f8725a77f9a09eba88daf1246
PHimmel <peterhimmelman@yahoo.com>
2021-01-20 19:58:42 -0500

    wip

--- single_matrix_control.py
+++ single_matrix_control.py
@@ -34,14 +34,19 @@ class Bash:
     def __init__(self):
         self.get_weather = 'WEATHER=$(curl wttr.in/13413?format="%C+%t+%hRH")'
 
-        self.scroll_weather = 'sudo ./scrolling-text-example $WEATHER -f ../fonts/peters_myfont.bdf --led-cols=64 ' \
-                              '--led-slowdown-gpio=8 -y-5 -b50 -s2 -l2 -C50,127,168'
+        self.scroll_weather = 'sudo ./scrolling-text-example $WEATHER -f ../fonts/PETERS_FONTS/joystix_17.bdf ' \
+                              '--led-cols=64 ' \
+                              '--led-rows=64 --led-chain=4 --led-parallel=2 --led-slowdown-gpio=8 -y-5 -b50 -s2 -l2 ' \
+                              '-C50,127,168 '
 
-        self.scroll_news = 'sudo ./scrolling-text-example $NEWS -f ../fonts/peters_myfont.bdf --led-pwm-bits=6 ' \
-                           '--led-cols=64 --led-slowdown-gpio=8 -B21,17,122 -C201,64,10 -y-5 -s2 -b60 -l1 '
+        self.scroll_news = 'sudo ./scrolling-text-example $NEWS -f ../fonts/PETERS_FONTS/joystix_17.bdf  ' \
+                           '--led-pwm-bits=6 ' \ 
+                           '--led-cols=64 --led-rows=64 --led-chain=4 --led-parallel=2 --led-slowdown-gpio=8 -B21,17,' \
+                           '122 -C201,64,10 -y-5 -s2 -b60 -l1 '
 
-        self.text = 'sudo ./scrolling-text-example 30 Minute Update! -f ../fonts/peters_myfont.bdf --led-cols=64 ' \
-                    '--led-slowdown-gpio=8 -b 50 -s2 -y-4 -B200,10,53 -l2'
+        self.text = 'sudo ./scrolling-text-example 30 Minute Update! -f ../fonts/PETERS_FONTS/joystix_17.bdf  ' \
+                    '--led-cols=64 ' \ 
+                    '--led-rows=64 --led-chain=4 --led-parallel=2 --led-slowdown-gpio=8 -b 50 -s2 -y-4 -B200,10,53 -l2'
 
         self.stop_matrix = 'ps -ef|grep led | grep -v grep | awk \'{print $2}\'| sudo xargs kill'
 
@@ -79,7 +84,8 @@ class Bash:
 
     def demo_number(self, number, time):
         self._call(
-            'sudo ./demo -D{} -t{} --led-cols=64 --led-rows=32 --led-brightness=50 --led-slowdown-gpio=2 -m 45'.format(
+            'sudo ./demo -D{} -t{} --led-cols=64 --led-rows=64 --led-brightness=50 --led-cols=64 --led-rows=64 '
+            '--led-chain=4 --led-parallel=2 --led-slowdown-gpio=8 -m 45'.format(
                 number,
                 time))
 

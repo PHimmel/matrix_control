77a9a6ec168216243e439f37e2da7fa389220f8b
Peter <peterhimmelman@yahoo.com>
2021-05-24 18:14:41 -0400

    wip

--- single_matrix_control.py
+++ single_matrix_control.py
@@ -34,40 +34,39 @@ from datetime import datetime
 from random import randint
 import basic_scraper
 
+# global variables
+matrix_settings = ' --led-cols=64 --led-rows=64 --led-chain=4 --led-parallel=2 --led-pwm-bits=6 --led-slowdown-gpio=8' \
+                  ' --led-pwm-lsb-nanoseconds=100 -f ../fonts/PETERS_FONTS/joystix_17.bdf --led-brightness=80 '
+clock_position = 'I:%M:%S_%p -x15 -y27'
+
+led_example_directory_path = '/home/pi/Matrix/ArduinoOnPc-FastLED-GFX-LEDMatrix/rpi-rgb-led-matrix/examples-api-use'
+text_cmd = 'scrolling-text-example'
+
 
 class Bash:
 
     def __init__(self):
         self.get_weather = 'WEATHER=$(curl wttr.in/13413?format="%C+%t+%hRH")'
 
-        self.scroll_weather = 'sudo ./scrolling-text-example $WEATHER -f ../fonts/PETERS_FONTS/joystix_17.bdf ' \
-                              '--led-cols=64 ' \
-                              '--led-rows=64 --led-chain=4 --led-parallel=2 --led-slowdown-gpio=8 -y25 -s3 -l2 ' \
-                              '-C50,127,168 '
+        self.scroll_weather = 'sudo ./{} $WEATHER -y25 -s3 -l2 -C50,127,168{}'.format(text_cmd, matrix_settings)
 
-        self.scroll_news = 'sudo ./scrolling-text-example $NEWS -f ../fonts/PETERS_FONTS/joystix_17.bdf ' \
-                           '--led-pwm-bits=6 ' \
-                           '--led-cols=64 --led-rows=64 --led-chain=4 --led-parallel=2 --led-slowdown-gpio=8 -B21,17,'\
-                           '122 -C201,64,10 -y24 -s6 -l1 '
+        self.scroll_news = 'sudo ./{} $NEWS -B21,17,122 -C201,64,10 -y24 -s6 -l1{}'.format(text_cmd, matrix_settings)
 
-        self.text = 'sudo ./scrolling-text-example 30 Minute Update! -f ../fonts/PETERS_FONTS/joystix_17.bdf ' \
-                    '--led-cols=64 --led-pwm-bits=6 ' \
-                    '--led-rows=64 --led-chain=4 --led-parallel=2 --led-slowdown-gpio=8 -s3 -y24 -B200,10,53 -l2'
+        self.text = 'sudo ./{} 30 Minute Update! -s3 -y24 -B200,10,53 -l2{}'.format(text_cmd, matrix_settings)
 
         self.stop_matrix = 'ps -ef|grep led | grep -v grep | awk \'{print $2}\'| sudo xargs kill'
 
-        self.led_example_directory_path = '/home/pi/Matrix/ArduinoOnPc-FastLED-GFX-LEDMatrix/rpi-rgb-led-matrix' \
-                                          '/examples-api-use'
+    @staticmethod
+    def _call(command):
+        call('{} > /dev/null 2>&1'.format(command), cwd=led_example_directory_path, shell=True)
 
-    def _call(self, command):
-        call('{} > /dev/null 2>&1'.format(command), cwd=self.led_example_directory_path, shell=True)
+    @staticmethod
+    def _popen(command):
+        Popen(command.split(' '), cwd=led_example_directory_path, stdout=DEVNULL)
 
-    def _popen(self, command):
-        Popen(command.split(' '), cwd=self.led_example_directory_path, stdout=DEVNULL)
-
-    def two_commands(self, one, two):
-        call('{} > /dev/null 2>&1 ; {} > /dev/null 2>&1'.format(one, two), cwd=self.led_example_directory_path,
-             shell=True)
+    @staticmethod
+    def two_commands(one, two):
+        call('{} > /dev/null 2>&1 ; {} > /dev/null 2>&1'.format(one, two), cwd=led_example_directory_path, shell=True)
 
     def kill_matrix(self):
         self._call(self.stop_matrix)
@@ -90,9 +89,7 @@ class Bash:
 
     def demo_number(self, number, time):
         self._call('bash -c \'sleep {} && sudo pkill -f demo\' &'.format(time))
-        self._call(
-            'sudo ./demo -D{} --led-cols=64 --led-rows=64 --led-brightness=50 --led-pwm-lsb-nanoseconds=100 '
-            '--led-chain=4 --led-pwm-bits=7 --led-parallel=2 --led-slowdown-gpio=8 -m 45'.format(number))
+        self._call('sudo ./demo -D{}{}-m 45'.format(number, matrix_settings[0:-60]))
 
 
 """
@@ -101,12 +98,8 @@ Clock offers user-specified default settings for the operation of an on-going cl
 
 
 class Clock(Bash):
-    single_clock = 'sudo ./clock -f ../fonts/PETERS_FONTS/joystix_17.bdf -d%-1I:%M:%S_%p --led-cols=64 --led-rows=64 '\
-                   '--led-chain=4 --led-pwm-bits=6 --led-pwm-lsb-nanoseconds=100 ' \
-                   '--led-parallel=2 --led-slowdown-gpio=8 -x15 -y27 '
-    double_clock = 'sudo ./clock -f ../fonts/PETERS_FONTS/joystix_17.bdf -d%I:%M:%S_%p --led-cols=64 --led-rows=64 '\
-                   '--led-chain=4 --led-pwm-bits=6 --led-pwm-lsb-nanoseconds=100 ' \
-                   '--led-parallel=2 --led-slowdown-gpio=8 -x15 -y27 '
+    single_clock = 'sudo ./clock -d %-1{}{}'.format(clock_position, matrix_settings)
+    double_clock = 'sudo ./clock -d %{}{}'.format(clock_position, matrix_settings)
 
     # nice ASCII --->   » ‗ ┼
 
@@ -118,16 +111,25 @@ class Clock(Bash):
         self.target_hour = 9
         self.hour, self.minute, self.second = get_current_hour_minute_second()
         self.regular_night = regular_night
+        self.nightClock = False
 
     # sets clock formatting based on number of positions required
     def set_clock(self, settings):
         if 0 < self.hour < 10 or 12 < self.hour < 22:
-            self.clock(Clock.single_clock + settings)
+            if self.nightClock is False:
+                self.clock(Clock.single_clock + settings)
+            else:
+                self.clock(Clock.single_clock[0:-20] + settings)
             return
-        self.clock(Clock.double_clock + settings)
+
+        if self.nightClock is False:
+            self.clock(Clock.double_clock + settings)
+        else:
+            self.clock(Clock.single_clock[0:-20] + settings)
 
     def night_clock(self):
         self.kill_matrix()
+        self.nightClock = True
 
         self.set_clock('-C 115,50,122 --led-brightness=2 ')
         self.set_sleep_till_hour()
@@ -135,19 +137,14 @@ class Clock(Bash):
     def set_sleep(self):
         # process start time
         start_secs = self.minute * 60 + self.second
-
         # current time
         now_secs = datetime.now().minute * 60 + datetime.now().second
-
         # time elapsed during process
         start_secs = now_secs - start_secs
-
         # total time until wake
         target_secs = self.target_minute * 60
-
         # seconds between process start and wake (ULTIMATELY ROUNDS TO THE NEAREST MINUTE)
         difference = target_secs - start_secs
-
         # sleep occurs in this method
         sleep(difference)
 
@@ -173,6 +170,7 @@ class Clock(Bash):
         sleep(slumber)
 
     def set_rand_color_clock_and_sleep(self):
+        self.nightClock = False
         self.set_clock(rand_color())
         self.set_sleep()
 
@@ -183,7 +181,7 @@ class Clock(Bash):
         if len(self.headlines) != 0:
             try:
                 self.message.run_messages(' >>> '.join(self.headlines[0:5]))
-                del(self.headlines[0:5])
+                del (self.headlines[0:5])
             except IndexError:
                 self.update_headlines_and_run_messages()
         else:
@@ -235,12 +233,15 @@ class Clock(Bash):
                 # day settings
                 if self.minute % 30 == 0:
                     self.kill_matrix()
+
                     Graphics().demo_number(10, 20)
                     self.kill_matrix()
                     self.run_messages_with_headlines()
+
                     # run specific demo for fixed period of time
                     Graphics().demo_number(9, 30)
                     self.kill_matrix()
+
                     self.set_rand_color_clock_and_sleep()
                     continue
 
@@ -297,9 +298,17 @@ def get_current_hour_minute_second():
     return now.hour, now.minute, now.second
 
 
+def rand_num():
+    x = 0
+    ls = []
+    while x < 3:
+        ls.append(randint(0, 255))
+        x += 1
+    return str(ls).strip('[]').replace(' ', '')
+
+
 def rand_color():
-    return '-C {},{},{} -B {},{},{} -O 0,0,0 --led-brightness=70 ' \
-        .format(randint(0, 255), randint(0, 255), randint(0, 255), randint(0, 255), randint(0, 255), randint(0, 255))
+    return '-C {} -B {} -O 0,0,0 '.format(rand_num(), rand_num())
 
 
 def main():
